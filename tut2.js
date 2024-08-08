1736d5d9c71079246fb19c8b5ce26ce77df8b15c
Ayush H <jqghebri@gmail.com>
2024-08-08 16:45:05 +0530

    creation

--- tut2.js
+++ tut2.js
@@ -1,1 +1,13 @@
-setTimeout()
+const sayArgus = function () {
+  console.log("Argus");
+}
+const changeText = function () {
+    document.querySelector('h1').innerHTML = "Ghost Over and Out!";
+}
+
+const changeMe = setTimeout(changeText, 2000);
+
+document.querySelector('#stop').addEventListener('click', function () {
+    clearTimeout(changeMe)
+    console.log("Timeout cleared");
+})
