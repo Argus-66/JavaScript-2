2a5e60a1189019ee3272b455ea97fffaccc7d426
Ayush H <jqghebri@gmail.com>
2024-08-08 11:13:07 +0530

    creation

---
+++ tut1.js
@@ -0,0 +1,7 @@
+// document.getElementById('owl').onclick = function() {
+//     alert('You clicked on the Owl!');
+// }
+
+document.getElementById('owl').addEventListener('click', function() {
+    alert('You clicked on the Owl!');
+})
